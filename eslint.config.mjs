0de77aa56b495020da209e55ba8ee7731f3003b2
MarcinSkic <marcinskicoficjal@gmail.com>
2024-07-31 14:53:53 +0200

    feat: install all required shadcn components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,9 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
+}, {
+  files: ['**/*.?([cm])[jt]s?(x)'],
+  rules: {
+    'react-refresh/only-export-components': ['off'],
+  },
 })
